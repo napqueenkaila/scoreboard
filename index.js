32ba1cb98c2f6173278939b584794171cd5237ee
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-07-07 16:54:14 -0400

    updates/finishing touches

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ let homeScoreBtnThree = document.getElementById("home-score-btn-3")
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
 
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0
+
+let resetBtn = document.getElementById("reset-btn")
+
 function increaseHomeScoreOne() {
     homeScore += 1
     homeScoreEl.textContent = homeScore
@@ -18,12 +26,6 @@ function increaseHomeScoreThree() {
     homeScore += 3
     homeScoreEl.textContent = homeScore
 }
-
-let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
-let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
-let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
-let guestScoreEl = document.getElementById("guest-score")
-let guestScore = 0
 
 function increaseGuestScoreOne() {
     guestScore += 1
@@ -40,3 +42,8 @@ function increaseGuestScoreThree() {
     guestScoreEl.textContent = guestScore
 }
  
+
+function resetGame() {
+    homeScoreEl.textContent = "0"
+    guestScoreEl.textContent = "0"
+}
