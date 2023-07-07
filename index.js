54bf47097dc83912839e146d3c3de180d84f4f2a
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-07-07 17:05:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-let homeScoreBtnOne = document.getElementById("home-score-btn-1")
-let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
-let homeScoreBtnThree = document.getElementById("home-score-btn-3")
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
 
-let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
-let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
-let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
 let guestScoreEl = document.getElementById("guest-score")
 let guestScore = 0
 
