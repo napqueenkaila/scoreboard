1228e7068ac89786b314d848350768d4533ab8de
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-07-07 17:26:36 -0400

    fix js

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-let homeScoreEl = document.getElementById("home-score")
+const homeScoreEl = document.getElementById("home-score")
+const guestScoreEl = document.getElementById("guest-score")
 let homeScore = 0
-
-let guestScoreEl = document.getElementById("guest-score")
 let guestScore = 0
 
-let resetBtn = document.getElementById("reset-btn")
-
-function increaseHomeScoreOne() {
+document.getElementById("home-score-1").addEventListener("click", () => {
     homeScore += 1
     homeScoreEl.textContent = homeScore
-}
+})
 
-function increaseHomeScoreTwo() {
+document.getElementById("home-score-2").addEventListener("click", () => {
     homeScore += 2
     homeScoreEl.textContent = homeScore
-}
+})
 
-function increaseHomeScoreThree() {
+document.getElementById("home-score-3").addEventListener("click", () => {
     homeScore += 3
     homeScoreEl.textContent = homeScore
-}
+})
 
-function increaseGuestScoreOne() {
+document.getElementById("guest-score-1").addEventListener("click", () => {
     guestScore += 1
     guestScoreEl.textContent = guestScore
-}
+})
 
-function increaseGuestScoreTwo() {
+document.getElementById("guest-score-2").addEventListener("click", () => {
     guestScore += 2
     guestScoreEl.textContent = guestScore
-}
+})
 
-function increaseGuestScoreThree() {
+document.getElementById("guest-score-3").addEventListener("click", () => {
     guestScore += 3
     guestScoreEl.textContent = guestScore
-}
- 
+})
 
-function resetGame() {
+document.getElementById("reset-btn").addEventListener("click", () => {
     homeScoreEl.textContent = "0"
     guestScoreEl.textContent = "0"
-}
+})
